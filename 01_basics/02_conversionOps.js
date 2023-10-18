036207ce620444a08ada3cf69a460198a06c2e72
Harshalshinkar <harshalshinkar@winjit.com>
2023-10-18 14:42:10 +0530

    added conversion file

---
+++ 01_basics/02_conversionOps.js
@@ -0,0 +1,43 @@
+let score = "30"
+
+let score1 = Number(score)
+// console.log(typeof score);
+// console.log(typeof score1);
+
+let score2 = parseInt(score)
+//console.log(typeof score2);
+
+
+let newScore = 30
+
+let newScore1 = String(newScore)
+// console.log(typeof newScore);
+// console.log(typeof newScore1);
+
+let isLoggedIn = true
+let newIsLoggedIn = Number(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(newIsLoggedIn);
+
+let newIsLoggedIn1 = String(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof newIsLoggedIn1);
+// console.log(newIsLoggedIn1);
+
+let val = null
+let newVal = Number(val)
+//console.log(typeof newVal);
+
+let val1 = undefined
+let newVal1 = Number(val1)
+//console.log(typeof newVal1);
+
+//***************************************
+
+let ops = "1"+5+2
+console.log(ops)
+console.log(typeof ops)
+
+let ops1 = 1+5+"2"
+console.log(ops1)
+console.log(typeof ops1)
