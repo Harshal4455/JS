9d5c54b649c029005ce8aaacf047230ca1024a05
Harshalshinkar <harshalshinkar@winjit.com>
2023-10-18 14:02:45 +0530

    Updated folder name

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+
+let str = "harshal"
+console.log(typeof str);
+
+let num = 10
+console.log(typeof num);
+
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+
+let temp = null //null => object
+console.log(typeof num);
+
+let state; // undefined => undefined
+console.log(typeof state);
+
